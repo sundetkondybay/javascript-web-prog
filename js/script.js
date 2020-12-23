d8194007c824f999aae566f5622999c539adab55
aCommonPerson <aCommonPerson0000@gmail.com>
2020-12-23 16:07:27 +0600

    js-2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 // 1 задание начало
 
-let name = prompt("ваше имя?");
-alert(name);
+/* let name = prompt("ваше имя?");
+alert(name); */
 
 // 1 задание конец
+
+
+
+// 2 задание начало
+
+let number = 10;
+
+nextPrime:
+for (let i = 2; i <= number; i++) { // Для всех i...
+
+   for (let check = 2; check < i; check++) { 
+      if (i % check == 0) continue nextPrime; 
+   }
+
+   console.log(i);
+}
+
+// 2 задание конец
+
+
+
+// 3 задание начало
+
+
+
+// 3 задание конец
